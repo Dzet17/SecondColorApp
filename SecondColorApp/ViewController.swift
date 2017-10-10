9f25aa44f81875e9bbfc466db99589de4f32f0e6
Dylan Zettler <dylanzettler@gmail.com>
2017-10-10 08:06:43 -0600

    Finished the Controller

--- SecondColorApp/ViewController.swift
+++ SecondColorApp/ViewController.swift
@@ -1,25 +1,50 @@
 //
 //  ViewController.swift
-//  SecondColorApp
+//  FirstSwiftProject
 //
-//  Created by Zettler, Dylan on 10/10/17.
+//  Created by Zettler, Dylan on 10/6/17.
 //  Copyright © 2017 CTEC. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var firstButton: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        if (firstButton.backgroundColor == .magenta)
+        {
+            firstButton.backgroundColor = .cyan
+        }
+        else
+        {
+            firstButton.backgroundColor = .magenta
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double (arc4random_uniform(256))/255/00)
+        let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
